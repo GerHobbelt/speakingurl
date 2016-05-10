72da3df75c06c59f5af9c40011d9cd7da5fd6de9
Mohammad Ali Talaghat <alitalaghat@gmail.com>
2016-05-10 16:52:41 +0430

    Update test-persian.js
    Adding numbers to persian test.

--- test/test-persian.js
+++ test/test-persian.js
@@ -1,14 +1,14 @@
 var getSlug = require('../lib/speakingurl');
 
-describe('getSlug translate arabic letters', function () {
+describe('getSlug translate persian letters/numbers', function () {
     'use strict';
 
     it('should be ', function (done) {
 
-        getSlug('گ چ پ ژ ک ی', {
-                lang: 'ar'
+        getSlug('گ چ پ ژ ک ی ۰ ۱ ۲ ۳ ۴ ۵ ۶ ۷ ۸ ۹', {
+                lang: 'fa'
             })
-            .should.eql('g-ch-p-zh-k-y');
+            .should.eql('g-ch-p-zh-k-y-0-1-2-3-4-5-6-7-8-9');
 
         done();
     });
